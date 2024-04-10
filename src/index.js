fa0a10d0e1d793611154e60905b1e8f62cf1e6e2
chris <cdiaz@launchdarkly.com>
2024-04-10 06:38:45 -0500

    gh pages url
    updated gh pages url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "661040b8213f790f88a566a9";
 
 let id = getUserId();
 
